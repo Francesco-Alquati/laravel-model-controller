9548acba75939971b7c9c4449f4774b0c933f435
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-09-26 03:27:22 +0200

    [UPD] Create layout and scaffolding

---
+++ resources/views/movies/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-cards">
+        <div class="row">
+                <div class="col-3 my-1 border bg-black">
+                    <div class="d-flex flex-column align-items-center justify-content-center text-center">
+                    <h6 class="mt-2 text-uppercase"></h6>
+                    <img src="" alt="" class="path">
+                    <h6 class="mt-3"></h6>
+                    <h6></h6>
+                    </div>
+                </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,9 @@
+<header>
+    <div class="container-fluid d-flex justify-content-center align-items-center">
+        <div class="row ">
+            <div class="col-12">
+                <h1 class="text-danger text-uppercase">Laraflix</h1>
+            </div>
+        </div>
+    </div>
+</header>
